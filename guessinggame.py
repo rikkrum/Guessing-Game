fa53bdf244c99fb7583609f109483b9c232c03ef
rikkrum <33042439+rikkrum@users.noreply.github.com>
2017-10-24 07:34:52 -0700

    Update guessinggame.py

--- guessinggame.py
+++ guessinggame.py
@@ -10,7 +10,7 @@ myName = input() # lets me input my name
 number = random.randint(1,20) # generates random numbers between range of 1-20, can be changed to whatever
 print('Well,' + myName + ', I am thinking of a number between 1 and 20. ')
 for guessesTaken in range(6): # range here limits how many guesses before game over. for loop 6 times begins
-    print('Take a guess.') # four spaces in front of print
+    print('Take a guess.')
 
     guess = input() # allows me to enter my guess
     guess = int(guess) # takes my input and make it into an integer
